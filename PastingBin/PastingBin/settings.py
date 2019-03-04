b88d01a70570ee254492fe123c579af450199c31
GitterAlex <junglejim1911@gmail.com>
2019-03-03 20:32:52 -0500

    Created framework

--- PastingBin/PastingBin/settings.py
+++ PastingBin/PastingBin/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'PastingBin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pastegres',
+        'USER': 'pastorjohn',
+        'PASSWORD': 'gresup',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
