82bced6d491c4b10617b77b68f54805ed965418e
Sam Campbell <campbellsam97@gmail.com>
2019-03-03 20:37:54 -0500

    Giving it a rub

--- PastingBin/PastingBin/settings.py
+++ PastingBin/PastingBin/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'PastingBin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pastegres',
+        'USER': 'pastorjohn',
+        'PASSWORD': 'gressup',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
