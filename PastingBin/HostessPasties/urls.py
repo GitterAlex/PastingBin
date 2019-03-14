8679d05934c37f3baacaa7814fd335772dc5755c
Sam Campbell <campbellsam97@gmail.com>
2019-03-14 14:08:55 -0400

    Started creating the ability to access short URLs

--- PastingBin/HostessPasties/urls.py
+++ PastingBin/HostessPasties/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from . import views
+from .models import PostTable
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
@@ -17,5 +18,6 @@ urlpatterns += [
     path('<int:pk>/update/', views.UserUpdate.as_view(), name='user_update'),
     path('<int:pk>/delete/', views.UserDelete.as_view(), name='user_delete'),
     path('<pk>/postdelete/', views.PostDelete.as_view(), name='post_delete'),
-    path('<pk>/postupdate/', views.PostUpdate.as_view(), name='post_update')
+    path('<pk>/postupdate/', views.PostUpdate.as_view(), name='post_update'),
+    path('<pk>/', views.PostView.as_view(), name='post_view')
 ]
