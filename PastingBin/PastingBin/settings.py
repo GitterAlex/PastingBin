12ace3c22fadd0680d585957a4fa17c40650b0ec
Sam Campbell <campbellsam97@gmail.com>
2019-03-15 21:49:52 -0400

    Created SSL Certification

--- PastingBin/PastingBin/settings.py
+++ PastingBin/PastingBin/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'HostessPasties.apps.HostesspastiesConfig'
+    'HostessPasties.apps.HostesspastiesConfig',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
